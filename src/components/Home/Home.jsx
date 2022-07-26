e711eaa4702e6312ed1c3e0fed8795d8464c536f
Kevin Roney <ktroney.pdx@gmail.com>
2022-07-25 17:31:20 -0700

    got initial build set up, going to add more during my exploration or during hack time

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable max-len */
 import styles from './Home.css';
 
 export default function Home() {
   return (
     <div className={styles.Home}>
-      <p>Home page stuff</p>
+      <h1>Home</h1>
+      <p>
+        Macaroni cheese the big cheese pecorino. Bocconcini pepper jack cheddar cheese triangles rubber cheese babybel cream cheese halloumi. Macaroni cheese pepper jack monterey jack stilton fondue fromage frais chalk and cheese cheeseburger. Gouda blue castello red leicester boursin.
+
+        Mozzarella st. agur blue cheese hard cheese. The big cheese camembert de normandie stilton pecorino airedale cheesy feet cream cheese melted cheese. Pepper jack jarlsberg cheese and wine cottage cheese cut the cheese taleggio the big cheese cheese and biscuits. Babybel.
+
+        Pepper jack taleggio cream cheese. Goat cream cheese cheeseburger queso stinking bishop cheesy grin danish fontina port-salut. Cheeseburger cheese and biscuits rubber cheese cauliflower cheese macaroni cheese bocconcini gouda airedale. Pecorino camembert de normandie cheddar cheesy feet bocconcini everyone loves roquefort camembert de normandie. Manchego taleggio.
+
+        Cheesy feet cheese strings emmental. Brie stilton hard cheese port-salut hard cheese danish fontina taleggio fromage frais. Paneer airedale hard cheese cheese and wine paneer caerphilly fromage fromage. Fromage cheese triangles cheese and wine everyone loves blue castello pepper jack st. agur blue cheese lancashire. Chalk and cheese parmesan.
+
+        Macaroni cheese emmental melted cheese. Port-salut st. agur blue cheese boursin croque monsieur cheese slices jarlsberg babybel when the cheese comes out everybody's happy. Bavarian bergkase lancashire cheese and biscuits mascarpone st. agur blue cheese everyone loves everyone loves swiss. Paneer chalk and cheese.
+      </p>
+
+
     </div>
   );
 }
