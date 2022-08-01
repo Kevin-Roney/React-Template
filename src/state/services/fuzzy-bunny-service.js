8dc7e17f3d0c67b33a293031b45797f7346d426f
Kevin Roney <ktroney.pdx@gmail.com>
2022-08-01 14:52:25 -0700

    added supabase client and fuzzy-bunny services

---
+++ src/state/services/fuzzy-bunny-service.js
@@ -0,0 +1,45 @@
+import { client } from './client.js';
+
+export async function getFamiliesWithBunnies() {
+  const response = await client.from('loving_families').select(`
+    id,
+    name,
+    avatar,
+    bunnies:fuzzy_bunnies(
+        id,
+        familyId:family_id,
+        name
+    )
+  `);
+
+  return response;
+}
+
+export async function removeFamily(id) {
+  const response = await client
+    .from('loving_families')
+    .delete()
+    .eq('id', id)
+    .single();
+
+  return response;
+}
+
+export async function addFamily(family) {
+  const response = await client
+    .from('loving_families')
+    .insert(family)
+    .single();
+
+  return response;
+}
+
+export async function updateFamily(family) {
+  const response = await client
+    .from('loving_families')
+    .update(family)
+    .eq('id', family.id)
+    .single();
+
+  return response;
+}
